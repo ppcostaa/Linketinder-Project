46f6d6aa6b03ed445054bd997abbc1cc2ef30c1e
Giovanna Costa <giovanna.portaldacosta@gmail.com>
2025-02-22 06:57:46 -0300

    feature/3: adicionando os arquivos relacionados as pessoas

---
+++ frontend/src/models/Candidato.ts
@@ -0,0 +1,26 @@
+import { Pessoa } from "./Pessoa";
+
+export class Candidato extends Pessoa {
+  cpf: string;
+  idade: number;
+  constructor(
+    nome: string,
+    email: string,
+    estado: string,
+    cep: string,
+    descricao: string,
+    competencias: string[],
+    cpf: string,
+    idade: number
+  ) {
+    super(nome, email, estado, cep, descricao, competencias);
+    this.cpf = cpf;
+    this.idade = idade;
+  }
+
+  toString(): string {
+    return `Candidato: ${this.getNome()}, ${this.getEmail()}, ${this.getEstado()}, ${this.getCep()}, ${this.getDescricao()}, ${this.getCompetencias()}, ${
+      this.cpf
+    }, ${this.idade}\n--------------------------------------`;
+  }
+}
